391ee97810f085183f74372922905aa9d23152b1
vikkoooo <vikkolundberg@gmail.com>
2024-06-13 14:30:39 +0200

    add remove search and get vehicles

--- GarageApp/Controller/GarageHandler.cs
+++ GarageApp/Controller/GarageHandler.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Diagnostics.Activity;
 
 namespace GarageApp.Controller
 {
@@ -19,7 +20,7 @@ namespace GarageApp.Controller
 			int size = 20;
 			garage = new Garage<Vehicle>(size); // set garage size at start (should be by user choice tho)
 			DevGenerateData(size);  // generate dummy vehicles with dummy values (fill half garage default) by calling DummyDataGenerator
-			PrintVehicles();
+			DevPrintVehicles();
 		}
 
 		// Methods
@@ -33,12 +34,7 @@ namespace GarageApp.Controller
 			}
 		}
 
-
-		// add vehicle to garage
-		// remove vehicle from garage (maybe by searching on the reg number)
-		// find a vehicle by reg number (remember .toupper)
-
-		public void PrintVehicles()
+		public void DevPrintVehicles()
 		{
 			foreach (var e in garage)
 			{
@@ -46,8 +42,32 @@ namespace GarageApp.Controller
 			}
 		}
 
+		// add vehicle to garage
+		public void Add(Vehicle v)
+		{
+			garage.Add(v); // todo: throw exception if error
+		}
+
+		// remove vehicle from garage (maybe by searching on the reg number instead??)
+		public void Remove(Vehicle v)
+		{
+			garage.Remove(v); // todo: throw exception if error
+		}
+
+		// find a vehicle by reg number (remember .toupper)
+		public Vehicle Search(string query)
+		{
+			// todo: throw exception if we get null object back
+			return garage.Search(query.ToUpper());
+		}
 
 		// list all current vehicles
+		// dont know if it should be IEnumerable or IEnumerator?
+		// I think IEnumerable is correct by https://stackoverflow.com/questions/619564/what-is-the-difference-between-ienumerator-and-ienumerable
+		public IEnumerable<Vehicle> GetVehicles()
+		{
+			return garage.ToList();
+		}
 		// list specific type of vehicle and the count of those
 		// find using linq "all black with 4 wheels", "all motorcycles pink 3 wheels", "all trucks", "all red"
 	}
