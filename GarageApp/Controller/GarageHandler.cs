ec940ced88f1e12be26f0f24d3885c28f6a769ec
vikkoooo <vikkolundberg@gmail.com>
2024-06-13 12:42:35 +0200

    brief todo

--- GarageApp/Controller/GarageHandler.cs
+++ GarageApp/Controller/GarageHandler.cs
@@ -8,5 +8,17 @@ namespace GarageApp.Controller
 {
 	internal class GarageHandler
 	{
+
+		// set garage size/cap at start (user choice)
+		// generate dummy vehicles with dummy values (fill half garage) by calling dummydatagenerator xxx times
+
+
+
+		// add vehicle to garage
+		// remove vehicle from garage (maybe by searching on the reg number)
+		// find a vehicle by reg number (remember .toupper)
+		// list all current vehicles
+		// list specific type of vehicle and the count of those
+		// find using linq "all black with 4 wheels", "all motorcycles pink 3 wheels", "all trucks", "all red"
 	}
 }
