9dc876e85a1a02c6e75156012e439872122205f0
vikkoooo <vikkolundberg@gmail.com>
2024-06-14 14:47:38 +0200

    change remove param and throw exceptions when not found

--- GarageApp/Model/Garage.cs
+++ GarageApp/Model/Garage.cs
@@ -33,17 +33,19 @@ namespace GarageApp.Model
 				data[count++] = element; // count is increased after the operation has processed
 		}
 
-		public void Remove(T element)
+		public void Remove(string regNumber)
 		{
 			for (int i = 0; i < count; i++)
 			{
-				if (data[i].Equals(element))
+				if (data[i].RegNumber.Equals(regNumber.ToUpper()))
 				{
 					data[i] = data[count - 1]; // move the last element of the array to current position to avoid a "hole" in the dataset
 					data[count - 1] = null; // clear last position to avoid confusion
 					count--; // we have now successfully overwritten the element we was looking for, decrease array count
+					return;
 				}
 			}
+			throw new KeyNotFoundException("Did not find any Vehicle with that registration number");
 		}
 
 		public T Search(string regNumber)
@@ -55,7 +57,7 @@ namespace GarageApp.Model
 					return data[i];
 				}
 			}
-			return null;
+			throw new KeyNotFoundException("Did not find any Vehicle with that registration number");
 		}
 
 		private bool IsFull()
